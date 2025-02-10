31fa236b8dc0b835e1dbd6ee07a0833fc83203ec
shyam <shyam18061996@gmail.com>
2025-02-10 18:03:00 +0530

    first commit

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Sparkling_Stars</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- custom css  -->
@@ -17,56 +17,66 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
         rel="stylesheet">
 
+
+    <!-- Lato google font  -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+
+
 </head>
 
 <body>
 
     <section>
         <div class="header">
-            <div class="container">
-                <div class="logo">
-                    <img src="./assets/images/SparkLogo.png" alt="logo_img">
+            <div class="container1">
+                <div class="nav">
+                    <div class="logo">
+                        <img src="./assets/images/SparkLogo.png" alt="logo_img">
+                    </div>
+
+                    <div class="menu-icon" onclick="toggleSidebar()">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+
+                    <div class="sidebar" id="sidebar">
+                        <div class="close-icon" onclick="toggleSidebar()">&times;</div>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="menu-icon" onclick="toggleSidebar()">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div class="imgSpark">
+                    <div class="planeSpark">
+                        <img src="./assets/images/SparkPlane.png" alt="logo_img">
+                    </div>
+                    <div class="leftside">
+                        <img src="./assets/images/SparkWave1.png" alt="logo_img">
+                    </div>
+                    <div class="rightside">
+                        <img src="./assets/images/SparkWave2.png" alt="logo_img">
+                    </div>
+                   
+
+                </div>
+                <div class="badge training ">Training and Mentorship</div>
+                <div class="netwokSpark">
+                        <img src="./assets/images/SparkNetwork.png" alt="logo_img">
+                    </div>
+                <div class="badge top-skill" style="background-color:#05d825;">Top Skill Courses</div>
+                <div class="bannerheading">
+                    <h1 class='d-md-block d-none'>YOUR FINANCIAL FREEDOM <br> AT YOUR FINGERTIPS</h1>
+                    <h1 class='d-md-none d-block'>YOUR <br> FINANCIAL FREEDOM <br> AT YOUR FINGERTIPS</h1>
+                    <img src="./assets/images/SparkGirl.png" class='d-md-block d-none' alt="logo_img">
+                    <img src="./assets/images/mobilegirl.png" class='d-md-none d-block' alt="logo_img">
                 </div>
 
-                <div class="sidebar" id="sidebar">
-                    <div class="close-icon" onclick="toggleSidebar()">&times;</div>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="sparkplane">
-                    <img src="./assets/images/SparkPlane.png" alt="logo_img">
-                </div>
-                <div class="sparkwave">
-                    <img src="./assets/images/SparkWave1.png" alt="logo_img">
-                </div>
 
-                <div class="headline">YOUR FINANCIAL FREEDOM <br> AT YOUR FINGERTIPS</div>
-                <div class="sparkwave1">
-                    <img src="./assets/images/SparkWave2.png" alt="logo_img">
-                </div>
-
-                <div class="sGirl d-flex justify-content-center">
-                    <img src="./assets/images/SparkGirl.png" alt="logo_img">
-                </div>
-                <div class="badge training">Training and Mentorship</div>
-                <div class="badge top-skill">Top Skill Courses</div>
-                <div class="network">
-                    <img src="./assets/images/SparkNetwork.png" alt="logo_img">
-                </div>
-
-                <div class="fram">
-                    <img src="./assets/images/SparkFram.png" alt="logo_img">
-                </div>
 
             </div>
         </div>
--- index.php
+++ index.php
@@ -1,5 +1,138 @@
 <?php include 'includes/header.php'; ?>
 
+<!-- Skills section start  -->
 
+<section class="skills">
+
+    <div class="col-12">
+        <div class="Skill_Heading">
+            <h1>SKILLS THAT MATTERS</h1>
+            <div class="SparkCircle ">
+                <img src="./assets/images/sprkcircle.png" alt="logo_img">
+            </div>
+            <div class="sparkBook d-md-block d-none">
+                <img src="./assets/images/SparkN.png" alt="logo_img">
+            </div>
+            <div class="spakb d-md-block d-none">
+                <img src="./assets/images/Spakbook.png" alt="logo_img">
+            </div>
+        </div>
+    </div>
+
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="Gold mb-3">
+                    <div class="gold-name">
+                        <h1>GOLD</h1>
+                    </div>
+                    <div class="skill-text mb-0">
+                        <h2>Online Marketing Mastery</h2>
+                        <p class='mt-2'>→</p>
+
+                    </div>
+                    <div class="price">
+                        <p><span>₹3510</span></p>
+                        <del style='color:#f9c900;'>
+                            <p><span>₹4375</span></p>
+                        </del>
+                    </div>
+                    <p class='desspak'>Seize this opportunity and start a journey of unprecedented growth by joining our
+                        Digital Marketing course.</p>
+                    <div class="imgs p-2 d-flex justify-content-center">
+                        <img src="./assets/images/gold.png" class='w-100' alt="logo_img">
+                    </div>
+                </div>
+                <button class='spakbuttom'>BUY NOW</button>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="dimond mb-3">
+                    <div class="diamond-name">
+                        <h1>DIAMOND</h1>
+
+                    </div>
+                    <div class="skill-text">
+                        <h2>Finance Mastery Course</h2>
+                        <p class='mt-2'>→</p>
+                    </div>
+                    <div class="price">
+                        <p><span>₹3510</span></p>
+                        <del style='color:#f9c900;'>
+                            <p><span>₹4375</span></p>
+                        </del>
+                    </div>
+                    <p class='desspak'>Seize this opportunity and start a journey of unprecedented growth by joining our
+                        Digital Marketing course.</p>
+                    <div class="imgs p-2 d-flex justify-content-center">
+                        <img src="./assets/images/diamond.png" class='w-100' alt="logo_img">
+                    </div>
+                </div>
+                <button class='spakbuttom'>BUY NOW</button>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="platinum mb-3">
+                    <div class="platinum-name">
+                        <h1>PLATINUM</h1>
+                    </div>
+                    <div class="skill-text">
+                        <h2>Data Science Mastery</h2>
+                        <p class='mt-2'>→</p>
+                    </div>
+                    <div class="price">
+                        <p><span>₹3510</span></p>
+                        <del style='color:#f9c900;'>
+                            <p><span>₹4375</span></p>
+                        </del>
+                    </div>
+                    <p class='desspak'>Seize this opportunity and start a journey of unprecedented growth by joining our
+                        Digital Marketing course.</p>
+                    <div class="imgs p-2 d-flex justify-content-center">
+                        <img src="./assets/images/platinum.png" class='w-100' alt="logo_img">
+                    </div>
+                </div>
+                <button class='spakbuttom'>BUY NOW</button>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Skills section End  -->
+
+
+<!-- providing section start -->
+
+<section class="providing">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center gap-2 py-5">
+            <div class='text-uppercase hgttyff'>What are we </div>
+            <div class='text-uppercase dfghjhgrtg'>Providing</div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-3">
+                <div class="proimg">
+                    <img src="./assets/images/proimg1.png" alt="logo_img">
+                </div>
+            </div>
+            <div class="col-md-3 mb-3">
+                <div class="proimg">
+                    <img src="./assets/images/proimg2.png" alt="logo_img">
+                </div>
+            </div>
+            <div class="col-md-3 mb-3">
+                <div class="proimg">
+                    <img src="./assets/images/proimg3.png" alt="logo_img">
+                </div>
+            </div>
+            <div class="col-md-3 mb-3">
+                <div class="proimg">
+                    <img src="./assets/images/proimg4.png" alt="logo_img">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- providing section end  -->
 
 <?php include 'includes/footer.php'; ?>
